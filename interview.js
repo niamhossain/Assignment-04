b13a45b2032db64937d62077c6caad5fd094648d
niamhossain <niamhossain469@gmail.com>
2026-03-07 00:31:20 +0600

    added functionality to all interview and rejected buttons and fixed function to append properly

--- interview.js
+++ interview.js
@@ -1,21 +1,62 @@
+// adding interview and rejected
 
-    // if accepted
-
-    function interview(interview,status,cardDiv){
-        const mblStatus = document.getElementById(status);
-        mblStatus.classList.remove('bg-[#EEF4FF]','btn-error');
-        mblStatus.classList.add('btn-success','text-white');
-
+function checkInterview(buttons,status,cardDivs){
+            const mblStatus = document.getElementById(status);
+            const button = document.getElementById(buttons);
+            const cardDiv = document.getElementById(cardDivs);
+            const cloneDiv = cardDiv.cloneNode(true);
+            cloneDiv.id = cardDivs + "clone"
+            let cloneStatus = cloneDiv.querySelector(".cursor-default");
+            const intSect = document.getElementById("interviewCards");
+            const rejSect = document.getElementById("rejectedCards");
+            const prevClone = document.getElementById(cardDivs + "clone");
+            if(button.innerText===mblStatus.innerText){
+                return;
+            }
+            else if(button.innerText==="INTERVIEW"){
+                mblStatus.classList.remove('bg-[#EEF4FF]','btn-error');
+                mblStatus.classList.add('btn-success','text-white');
+                mblStatus.innerText = "INTERVIEW";
+                cloneStatus.innerText = "INTERVIEW";
+                cloneStatus.classList.remove('bg-[#EEF4FF]','btn-error');
+                cloneStatus.classList.add('btn-success','text-white');
+                if(prevClone){
+                    prevClone.remove();
+                }
+                intSect.append(cloneDiv);
+            }
+            else if(button.innerText==="REJECTED"){
+                mblStatus.classList.remove('bg-[#EEF4FF]','btn-success');
+                mblStatus.classList.add('btn-error','text-white');
+                mblStatus.innerText = "REJECTED";
+                cloneStatus.innerText = "REJECTED";
+                cloneStatus.classList.remove('bg-[#EEF4FF]','btn-success');
+                cloneStatus.classList.add('btn-error','text-white');
+                if(prevClone){
+                    prevClone.remove();
+                }
+                rejSect.append(cloneDiv);
+            }
     }
 
 
-    // if rejected
-    function rejected(rejected,status,cardDiv){
-        const mblStatus = document.getElementById(status);
-        mblStatus.classList.remove('bg-[#EEF4FF]','btn-success');
-        mblStatus.classList.add('btn-error','text-white')
-    }
+    // Switching sections
 
-    function checkInterview(check,status,cardDiv){
-
-    }
+    const allBtn = document.getElementById("allButton");
+    const intBtn = document.getElementById("interviewButton");
+    const rejBtn = document.getElementById("rejectedButton");
+    allBtn.addEventListener("click", function(){
+        document.getElementById("intSect").classList.add("hidden");
+        document.getElementById("rejSect").classList.add("hidden");
+        document.getElementById("allSect").classList.remove("hidden");
+    })
+    intBtn.addEventListener("click", function(){
+        document.getElementById("allSect").classList.add("hidden");
+        document.getElementById("rejSect").classList.add("hidden");
+        document.getElementById("intSect").classList.remove("hidden");
+    })
+    rejBtn.addEventListener("click", function(){
+        document.getElementById("intSect").classList.add("hidden");
+        document.getElementById("allSect").classList.add("hidden");
+        document.getElementById("rejSect").classList.remove("hidden");
+    })
